989bb09bcc373c6d74be041e7d786b7e58f99ab9
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2023-04-06 20:18:54 +0100

    power app project finished

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const fetchPower = () => {
                 .forEach((source, i) => {
                     document.getElementById(`power-desc-${i + 1}`)
                         .textContent = `${source.fuel}: ${source.perc}%`
-                    document.getElementById(`power-${i + 1}`)
-                        .style.setProperty("--power-percentage", `${source.perc}%`)
+                    document.getElementById(`bar-${i + 1}`)
+                        .style.setProperty("width", `${source.perc}%`)
                     document.getElementById(`icon-${i+1}`)
                         .classList.add(`${icons[source.fuel][0]}`, `${icons[source.fuel][1]}`)
                 })
@@ -28,11 +28,39 @@ const fetchPower = () => {
 }
 fetchPower()
 
+const indexIcon = {
+    'very low': ['fa-regular', 'fa-smile', 'darkgreen'],
+    'low': ['fa-regular', 'fa-smile', 'darkgreen'],
+    'moderate': ['fa-regular', 'fa-meh', 'darkorange'],
+    'high': ['fa-regular', 'fa-frown', 'red'],
+    'very high': ['fa-regular', 'fa-frown', 'red']
+}
 
 fetch(`${baseUrl}intensity`).then((response) => response.json())
     .then((data) => {
-        data.data[0].intensity
+        const indexToday = data.data[0].intensity
+        document.getElementById('indexIcon')
+            .classList.add(`${indexIcon[indexToday.index][0]}`, `${indexIcon[indexToday.index][1]}`)
+        document.getElementById('indexWord')
+            .innerText = `${indexToday.index}`
+        document.getElementById('indexNumber')
+            .innerText = `${indexToday.actual}g/kWh`
+        document.getElementById('indexIcon')
+            .style.color = `${indexIcon[indexToday.index][2]}`
+        document.getElementById('indexWord')
+            .style.color = `${indexIcon[indexToday.index][2]}`
     })
+
+
+const infoBtn = document.getElementById('infoBtn')
+infoBtn.onclick = () => {
+    const infoBox = document.getElementById('infoBox')
+        infoBox.classList.toggle('reveal')
+    const infoText = document.getElementById('infoText')
+    infoText.classList.toggle('show')
+    const thisBtn  = document.getElementById('infoBtn')
+    thisBtn.classList.toggle('on')
+}
 
 
 
