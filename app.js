5395364cf12a83a9c3ca9ae7efa2c0799350112f
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2023-04-06 09:39:10 +0100

    added basic power bars without styling

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-const urlPowerMix = 'https://api.carbonintensity.org.uk/generation'
+const baseUrl = 'https://api.carbonintensity.org.uk/'
 
-let currentIntensity
-
+const icons = {
+    gas: ['fa-solid', 'fa-fire'],
+    wind: ['fa-solid', 'fa-wind'],
+    nuclear: ['fa-solid', 'fa-circle-radiation'],
+    imports: ['fa-solid', 'fa-ship'],
+    biomass: ['fa-solid', 'fa-tree'],
+    hydro: ['fa-solid', 'fa-water'],
+    solar: ['fa-solid', 'fa-sun'],
+    coal: ['fa-solid', 'fa-smog'],
+    other: ['fa-solid', 'fa-question']
+}
 
 const fetchPower = () => {
-    fetch(urlPowerMix).then((response) => response.json())
+    fetch(`${baseUrl}generation`).then((response) => response.json())
         .then((data) => {
             data.data.generationmix.sort((a, b) => b.perc - a.perc)
                 .forEach((source, i) => {
-                    document.getElementById(`power-desc-${i+1}`)
+                    document.getElementById(`power-desc-${i + 1}`)
                         .textContent = `${source.fuel}: ${source.perc}%`
+                    document.getElementById(`power-${i + 1}`)
+                        .style.setProperty("--power-percentage", `${source.perc}%`)
+                    document.getElementById(`icon-${i+1}`)
+                        .classList.add(`${icons[source.fuel][0]}`, `${icons[source.fuel][1]}`)
                 })
         })
 }
 fetchPower()
 
-const urlCurrentIntensity = 'https://api.carbonintensity.org.uk/intensity'
-// export let currentIntensity
 
-fetch(urlCurrentIntensity).then((response) => response.json())
+fetch(`${baseUrl}intensity`).then((response) => response.json())
     .then((data) => {
-        currentIntensity = data.data[0].intensity
+        data.data[0].intensity
     })
 
 
